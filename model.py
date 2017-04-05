64cc11ea5183e0c6baec10151839f44d19a81862
sk <lorez@mail.ru>
2017-04-05 09:52:21 +0500

    New classes: Group Fee (prototype) Entry (prototype) RelayTeam (prototype)
    Added some fields:
    Course: race, control_text
    CourseControl: is_radio, status
    Person: year, rank, qaul
    ControlCard: is_rented, is_returned
    Start: comment, competing_status, entry, relay_team, relay_leg, start_group
    Result: result, place, penalty_time, penalty_laps

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from playhouse.migrate import *
 from peewee import *
 
 database_proxy = Proxy()
@@ -20,6 +19,8 @@ class Course(BaseModel):
     length = DoubleField(null=True)
     climb = DoubleField(null=True)
     number_of_controls = DoubleField(null=True)
+    race = ForeignKeyField(Race)  # connection to the race
+    controls_text = TextField()  # JSON with legs and controls. If you have 500 courses for relay, it will be faster
 
 
 class CourseControl(BaseModel):
@@ -28,6 +29,8 @@ class CourseControl(BaseModel):
     map_text = TextField(null=True)
     leg_length = DoubleField(null=True)
     score = DoubleField()
+    is_radio = TextField()  # specify if the control is used as radio/TV control
+    status = TextField()  # enabled / disabled - e.g. was stolen, broken
 
 
 class RaceStatus(BaseModel):
@@ -86,7 +89,7 @@ class Address(BaseModel):
 
 class Race(BaseModel):
     name = TextField()
-    start_time = DateTimeField()
+    start_time = DateTimeField()  # full date, including the day e.g. 2017-03-07 11:00:00
     end_time = DateTimeField()
     status = ForeignKeyField(RaceStatus)
     url = TextField(null=True)
@@ -101,28 +104,42 @@ class Team(BaseModel):
 class Person(BaseModel):
     name = TextField()
     surname = TextField(null=True)
+    year = SmallIntegerField()  # sometime we have only year of birth
     birth_date = DateField(null=True)
     team = ForeignKeyField(Team, null=True)
     nationality = ForeignKeyField(Country, null=True)
     address = ForeignKeyField(Address, null=True)
     contact = ForeignKeyField(Contact, null=True)
-    world_code = TextField() # WRE ID for orienteering and the same
+    world_code = TextField()  # WRE ID for orienteering and the same
     national_code = TextField()
+    rank = DecimalField()  # position/scores in word ranking
+    qual = ForeignKeyField(Qualification)  # qualification, used in Russia only
 
 
 class ControlCard(BaseModel):
     person = ForeignKeyField(Person)
     name = CharField()
     value = TextField()
+    is_rented = BooleanField()  # either card is own or should be returned
+    is_returned = BooleanField()  # used to control the returning of rented cards
 
 
 class Start(BaseModel):
-    race = ForeignKeyField(Race)
+    group = ForeignKeyField(Group)  # changed from course by SK
     person = ForeignKeyField(Person)
     # TODO: control_card = ForeignKeyField(Person)
     course = ForeignKeyField(Course)
     bib_number = CharField()
     start_time = DateTimeField()
+    comment = TextField()  # comment (taken from the entry or entered manually)
+    competing_status = TextField()  # can de a dictionary?
+    # usual
+    # not_ranked - out of team, not to give scores (e.g. team consist of 6 persons, 7th and 8th are not ranked)
+    # not_qualified - won't be in official results
+    entry = ForeignKeyField(Entry)  # connection with the Entry object
+    relay_team = ForeignKeyField(RelayTeam)  # conection with relay team.
+    relay_leg = IntegerField()  # relay leg. Possible to calculate it from the bib number
+    start_group = IntegerField()  # used in drawing, to specify red/ping and other start groups
 
 
 class Result(BaseModel):
@@ -131,3 +148,43 @@ class Result(BaseModel):
     status = ForeignKeyField(ResultStatus)
     split_time = TextField(null=True)
     control_card = ForeignKeyField(ControlCard)
+    result = TextField()  # it's better to store calculated result to do fast return in case of mass requests
+    place = IntegerField()  # it's better to store calculated place and update it after each readout / disqualification
+    penalty_time = DecimalField()  # time of penalties (marked route, false start)
+    penalty_laps = IntegerField()  # count of penalty legs (marked route)
+
+
+class Group(BaseModel):
+    """
+    Should have name 'Class' according to IOF spec, but this word is reserved in Python
+    """
+    name = TextField()  # short name, max 5-6 chars e.g. M17
+    long_name = TextField()  # to print in official results e.g. 'Juniors before 17 years"
+    course = ForeignKeyField(Course)
+
+    sex = TextField()  # limitation for entry
+    min_age = IntegerField()
+    max_age = IntegerField()
+
+    fee = ForeignKeyField(Fee)
+    max_time = IntegerField()  # max time for disqualification
+    qual_assign_text = TextField()  # JSON with rules for Qualification calculation (1:MS, 2-6:KMS etc.) - Russia only
+    start_interval = IntegerField()
+    start_corridor = IntegerField()  # number of start corridor
+    order_in_corridor = IntegerField()  # order in start corridor for automatic start time/bib calculation in all groups
+
+
+class Fee(BaseModel):
+    pass
+
+
+class Entry(BaseModel):
+    pass
+
+
+class RelayTeam(BaseModel):
+    pass
+
+
+class Qualification(BaseModel):
+    pass
