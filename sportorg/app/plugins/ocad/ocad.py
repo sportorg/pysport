74ea958608541aad1d2e7c1efd28699236b36a2a
Akhtarov Danil <danil77735@gmail.com>
2017-05-03 14:06:08 +0500

    Logging

--- sportorg/app/plugins/ocad/ocad.py
+++ sportorg/app/plugins/ocad/ocad.py
@@ -1,11 +1,15 @@
 from sportorg.lib.ocad import ocad
 from sportorg.app.models import model
 import time
+import logging
+import config
 
 
 def import_txt_v8(source):
     classes_v8 = ocad.parse_txt_v8(source)
     diff = time.time()
+    logging.basicConfig(**config.LOG_KWARGS, level=logging.DEBUG if config.DEBUG else logging.WARNING)
+    logging.info("Start")
 
     for course in classes_v8.courses:
         c = model.Course.create(
@@ -24,6 +28,6 @@ def import_txt_v8(source):
                     order=control.order,
                     leg_length=control.length
                 )
-    print(time.time()-diff)
+    logging.info("Finish " + str(time.time()-diff))
 
     return True
