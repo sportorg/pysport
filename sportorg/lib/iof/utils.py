2fef98185aff205e5a2350e35a7871a08b7e4542
Akhtarov Danil <danil77735@gmail.com>
2017-06-21 15:02:20 +0500

    IOFxml. Created func indent

---
+++ sportorg/lib/iof/utils.py
@@ -0,0 +1,23 @@
+
+
+def indent(elem, level=0):
+    """   
+        import xml.etree.ElementTree as ET
+        
+        elem = ET.Element('MyElem')
+        
+        indent(elem) 
+    """
+    i = "\n" + level*"\t"
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "\t"
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
