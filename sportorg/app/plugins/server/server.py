a44de677ac39e1c6ce4ba232204abcd209c0954c
Akhtarov Danil <danil77735@gmail.com>
2017-08-05 00:29:43 +0500

    Flask server for remote clients

---
+++ sportorg/app/plugins/server/server.py
@@ -0,0 +1,21 @@
+from threading import Thread
+from .view import *
+
+
+class ServerThread(Thread):
+    def run(self):
+        app.run()
+
+
+def run():
+    server = ServerThread()
+    server.start()
+
+    return server
+
+
+def stop():
+    pass
+
+if __name__ == '__main__':
+    app.run()
