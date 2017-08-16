1c95217c9f575b3b48289ccd44d31cfe595552dd
Akhtarov Danil <danil77735@gmail.com>
2017-08-16 22:50:46 +0500

    class App

---
+++ sportorg/core/app.py
@@ -0,0 +1,20 @@
+class App:
+
+    def __init__(self, options=None):
+        self.options = options
+        self.file = None
+
+    def create_file(self):
+        pass
+
+    def open_file(self):
+        pass
+
+    def save_file(self):
+        pass
+
+    def get_settings(self):
+        pass
+
+    def save_settings(self):
+        pass
