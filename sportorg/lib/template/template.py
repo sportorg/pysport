794f8e3c7f58913c6bc457775ea32213c0061d15
sk <lorez@mail.ru>
2017-09-19 14:44:53 +0500

    Loacle fixing for Jinja2. Note, that national encodings won't work if OS locale differs. e.g. to have Russian strings in Template you need to have CP1251, ISO-8859-5 or UTF-8 locale in OS.

--- sportorg/lib/template/template.py
+++ sportorg/lib/template/template.py
@@ -1,7 +1,12 @@
+import locale
+
 from jinja2 import Template
 
 
 def get_text_from_file(path, **kwargs):
-    html = open(path).read()
+    custom_encoding = locale.getdefaultlocale()[1]
+    # custom_encoding = 'cp1251'
+    html = open(path).read().encode(custom_encoding, 'ignore').decode()
+
     template = Template(html)
     return template.render(**kwargs)
