999b667a48897491b8b644dd4e2f23132ed4f6ff
sk <lorez@mail.ru>
2017-10-09 18:42:16 +0500

    Course export - encoding, converting to meters

--- sportorg/app/modules/ocad/ocad.py
+++ sportorg/app/modules/ocad/ocad.py
@@ -38,19 +38,19 @@ def import_txt_v8(source):
     for course in classes_v8.courses:
         c = memory.create(
             memory.Course,
-            course=course.type,
-            name=course.group,
-            length=course.length,
+            name=course.group if course.group else course.course,
+            length=int(course.length * 1000),
             climb=course.climb
         )
         controls = []
         for order, control in course.controls.items():
-            controls.append(memory.create(
-                memory.CourseControl,
-                code=control.code,
-                order=control.order,
-                length=control.length
-            ))
+            if str(control.code).isdecimal():  # don't use start and finish
+                controls.append(memory.create(
+                    memory.CourseControl,
+                    code=control.code,
+                    order=control.order,
+                    length=int(control.length * 1000)
+                ))
         c.controls = controls
         memory.race().courses.append(c)
 
