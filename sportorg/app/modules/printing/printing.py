ffbc1bce76c23a48a7f9d24fcbf1baa3f7297400
sk <lorez@mail.ru>
2017-10-31 00:05:58 +0500

    Automatic splits printing while SI-card readout

--- sportorg/app/modules/printing/printing.py
+++ sportorg/app/modules/printing/printing.py
@@ -1,13 +1,54 @@
 import logging
+from threading import Thread
 
-import sys
+from PyQt5.QtCore import QSizeF
 from PyQt5.QtGui import QTextDocument
 from PyQt5.QtPrintSupport import QPrinter
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
 from PyQt5.QtWidgets import QApplication
 
 
+class PrintThread(Thread):
+    def __init__(self, printer_name, html):
+        super().__init__()
+        self.printer_name = printer_name
+        self.html = html
+
+    def run(self):
+        app = QApplication.instance()
+        if app is None:
+            app = QApplication(['--platform', 'minimal'])
+        # we need this call to correctly render images...
+        app.processEvents()
+
+        printer = QPrinter()
+        if self.printer_name:
+            printer.setPrinterName(self.printer_name)
+
+        printer.setResolution(96)
+
+        text_document = QTextDocument()
+
+        printer.setFullPage(True)
+        printer.setPageMargins(5, 5, 5, 5, QPrinter.Millimeter)
+
+        page_size = QSizeF()
+        page_size.setHeight(printer.height())
+        page_size.setWidth(printer.width())
+        text_document.setPageSize(page_size)
+        text_document.setDocumentMargin(0.0)
+
+        text_document.setHtml(self.html)
+        text_document.print_(printer)
+
+
 def print_html(printer_name, html):
+    thread = PrintThread(printer_name, html)
+    thread.start()
+    logging.info('printing thread started')
+
+
+def print_html_webengine(printer_name, html):
     printer = QPrinter()
     if printer_name:
         printer.setPrinterName(printer_name)
@@ -31,19 +72,6 @@ def print_html(printer_name, html):
     text_document.setHtml(html)
 
 
-def main():
-    try:
-        printer_name = 'Adobe PDF'
-        printer = QPrinter()
-        printer.setPrinterName(printer_name)
-        text_document = QTextDocument()
-        text_document.setHtml("hello")
-        text_document.print_(printer)
-    except Exception as e:
-        logging.exception(e)
-
-
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main()
-    sys.exit(app.exec_())
+    print_html('Adobe PDF', "hello, printer")
+
