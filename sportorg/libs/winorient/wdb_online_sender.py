511c1acebc8d66a5331a22f74d9e5449babc2808
sk <lorez@mail.ru>
2018-01-11 12:11:54 +0500

    WDB monitoring and sending to online - orgeo.ru

---
+++ sportorg/libs/winorient/wdb_online_sender.py
@@ -0,0 +1,129 @@
+import threading
+
+import time
+import os
+import logging
+from pip._vendor import requests
+
+from sportorg.core.otime import OTime
+from sportorg.libs.winorient.wdb import WDB, parse_wdb, WDBFinish, WDBMan, WDBPunch
+from sportorg.utils.time import int_to_otime, time_to_hhmmss
+
+ONLINE_NUMBER = "num"
+ONLINE_RESULT = "r"
+ONLINE_TIME = "t"
+ONLINE_FLAG = "fl"
+ONLINE_LAP = "dubl"
+ONLINE_SPLIT = "s"
+ONLINE_PARAM1 = "lis"
+ONLINE_SCORE = "ochki"
+ONLINE_FINISH_TIME = "tfin"
+ONLINE_SI = "si"
+ONLINE_COMMENT = "com"
+
+class WdbOnlineSender(object):
+    def __init__(self):
+        self.url = ''
+        self.file_path = ''
+        self.send_splits = True
+        self.check_interval = 1000 # ms
+        self.is_running = False
+        self.last_modification_time = None
+        self.current_wdb = None
+        self.tr = None
+
+    def start(self):
+        self.tr = threading.Thread(target=self.run)
+        self.tr.start()
+
+    def run(self):
+        while True:
+            logging.info('thread iteration')
+            mod_time = time.ctime(os.path.getmtime(self.file_path))
+            if self.last_modification_time:
+                if mod_time != self.last_modification_time:
+                    self.read()
+            else:
+                self.read()
+            self.last_modification_time = mod_time
+            time.sleep(float(self.check_interval) / 1000)
+
+    def read(self):
+        wdb = parse_wdb(self.file_path)
+        new_finishes = self.get_new_finishes(wdb)
+        self.current_wdb = wdb
+        for finish in new_finishes:
+            self.send(finish)
+
+    def get_new_finishes(self, wdb):
+        ret = []
+        if wdb and isinstance(wdb, WDB):
+            for new_finish in wdb.fin:
+                if isinstance(new_finish, WDBFinish):
+                    bib = new_finish.number
+                    if bib:
+                        if self.current_wdb and isinstance(self.current_wdb, WDB):
+                            old_finish = self.current_wdb.find_finish_by_number(bib)
+
+                            if old_finish and isinstance(old_finish, WDBFinish):
+
+                                if old_finish.time != new_finish.time:
+                                    # result changed
+                                    ret.append(new_finish)
+                                    continue
+
+                                old_person = self.current_wdb.find_man_by_number(old_finish.number)
+                                new_person = wdb.find_man_by_number(new_finish.number)
+
+                                if old_person and new_person:
+                                    if old_person.status != new_person.status:
+                                        # status changed
+                                        ret.append(new_finish)
+                                        continue
+
+                            else:
+                                # new finish
+                                ret.append(new_finish)
+                                continue
+                        else:
+                            # first run, empty previous base
+                            ret.append(new_finish)
+                            continue
+        return ret
+
+    def send(self, finish):
+        if finish and isinstance(finish, WDBFinish):
+            man = self.current_wdb.find_man_by_number(finish.number)
+            assert isinstance(man, WDBMan)
+            ret = self.url
+            ret += ONLINE_NUMBER + "=" + str(finish.number)
+            ret += "&" + ONLINE_RESULT + "=" + time_to_hhmmss(int_to_otime(man.result))
+            ret += "&" + ONLINE_FLAG + "=" + str(man.status)
+            ret += "&" + ONLINE_TIME + "=" + str(OTime.now().to_sec() * 100) # WO-compatible time in sec * 100
+            if self.send_splits:
+                splits = self.get_splits(self.current_wdb, man)
+                ret += "&" + ONLINE_SPLIT + "=" + splits
+
+            logging.info(ret)
+            res = requests.get(ret)
+
+    def get_splits(self, wdb, man):
+        """
+             строка со сплитами. WinOrient передает её в закодированном бинарном формате
+             Формат: [SSSSSSTTTTTTCC]*
+             SSSSSS - номер чипа в 16-чной системе
+             TTTTTT - время в 16-чной системе
+             СС - номер КП в 16-чной системе
+        """
+        assert isinstance(wdb, WDB)
+        assert isinstance(man, WDBMan)
+
+        chip = man.get_chip()
+        separator = ''
+        ret = ''
+        for cur_punch in chip.punch:
+            assert isinstance(cur_punch, WDBPunch)
+            if cur_punch.code:
+                split = '{:06x}{:06x}{:02x}'.format(chip.id, int(cur_punch.time / 100), cur_punch.code)
+                ret += split + separator
+        return ret
---
+++ sportorg/libs/winorient/wdb_online_sender_dialog.py
@@ -0,0 +1,64 @@
+import logging
+import sys
+
+from PyQt5.QtWidgets import QFormLayout, QLabel, QLineEdit, QDialog, QPushButton, QApplication, QFileDialog, QTextEdit
+from sportorg.libs.winorient.wdb_online_sender import WdbOnlineSender
+
+
+class WdbOnlineSenderDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Online sender')
+        self.setSizeGripEnabled(False)
+        self.setModal(True)
+
+        self.layout = QFormLayout(self)
+
+        self.file_button = QPushButton('Select wdb file')
+        def select_file():
+            result = QFileDialog.getOpenFileName(None, 'select wdb file', '', 'WDB Winorient (*.wdb)')[0]
+            self.file_path.setText(result)
+        self.file_button.clicked.connect(select_file)
+        self.file_path = QLineEdit()
+        self.layout.addRow(self.file_button, self.file_path)
+
+        self.url_label = QLabel('URL')
+        # self.url_item = QLineEdit('http://orgeo.ru/online/sv?id=20000&sk=20000&sub=1&')
+        self.url_item = QLineEdit('http://orgeo.ru/online/sv?id=5651&sk=05c15&sub=1&')
+        self.url_item.setMinimumWidth(300)
+        self.layout.addRow(self.url_label, self.url_item)
+
+        self.log_area = QTextEdit()
+        self.layout.addRow(self.log_area)
+
+        def cancel_changes():
+            self.close()
+
+        def start():
+            try:
+                self.start()
+            except Exception as e:
+                logging.exception(str(e))
+
+        self.button_ok = QPushButton('Start')
+        self.button_ok.clicked.connect(start)
+        self.button_cancel = QPushButton('Cancel')
+        self.button_cancel.clicked.connect(cancel_changes)
+        self.layout.addRow(self.button_ok, self.button_cancel)
+
+        self.online_sender = WdbOnlineSender()
+
+        self.show()
+
+    def start(self):
+        file_path = self.file_path.text()
+        url = self.url_item.text()
+
+        self.online_sender.file_path = file_path
+        self.online_sender.url = url
+        self.online_sender.start()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    logging.basicConfig(level=logging.DEBUG)
+    WdbOnlineSenderDialog().exec()
