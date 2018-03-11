dea9b502616ab5ac58d0893c335229d91562dac4
Akhtarov Danil <danil77735@gmail.com>
2018-03-11 19:32:26 +0500

    Setting environment for jinja

--- sportorg/core/template.py
+++ sportorg/core/template.py
@@ -4,21 +4,32 @@ from sportorg.libs.template import template
 from sportorg import config
 
 
-def get_templates(path=None):
-    if path is None:
+def get_templates(path='', exclude_path=''):
+    if not path:
         path = config.template_dir()
+    if not exclude_path:
+        exclude_path = config.template_dir()
     files = []
     for p in os.listdir(path):
         full_path = os.path.join(path, p)
         if os.path.isdir(full_path):
             fs = get_templates(full_path)
             for f in fs:
+                f = f.replace(exclude_path, '')
+                f = f.replace('\\', '/')
                 files.append(f)
         else:
+            full_path = full_path.replace(exclude_path, '')
+            full_path = full_path.replace('\\', '/')
             files.append(full_path)
 
     return files
 
 
 def get_text_from_file(path, **kwargs):
-    return template.get_text_from_file(path, **kwargs)
+    kwargs['name'] = config.NAME
+    kwargs['version'] = str(config.VERSION)
+    if os.path.isfile(path):
+        return template.get_text_from_path(path, **kwargs)
+    else:
+        return template.get_text_from_template(config.template_dir(), path, **kwargs)
