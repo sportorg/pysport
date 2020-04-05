1b20eb29d6a4f7f35d2d436f75987490e3a91b4b
sergeikobelev <lorez@mail.ru>
2020-04-05 14:12:21 +0500

    fixed - calculate places for relative (cumulative from start) times

--- sportorg/models/result/split_calculation.py
+++ sportorg/models/result/split_calculation.py
@@ -188,24 +188,41 @@ class GroupSplits(object):
         if not len(self.person_splits):
             return
 
-        leader_time = self.person_splits[0].get_leg_time(index)
-        
-        double_places_counter = 0
-        prev_split_time = leader_time
-        for i in range(len(self.person_splits)):
-            person = self.person_splits[i]
-            leg = person.get_leg_by_course_index(index)
-            if leg:
-                if i != 0 and prev_split_time == leg.leg_time:
-                    double_places_counter += 1
-                else:
-                    double_places_counter = 0
-                if relative:
+        if relative:
+            # calculate places for relative (cumulative from start) times
+            leader_time = self.person_splits[0].get_leg_relative_time(index)
+
+            double_places_counter = 0
+            prev_split_time = leader_time
+            for i in range(len(self.person_splits)):
+                person = self.person_splits[i]
+                leg = person.get_leg_by_course_index(index)
+                if leg:
+                    if i != 0 and prev_split_time == leg.relative_time:
+                        double_places_counter += 1
+                    else:
+                        double_places_counter = 0
+
                     leg.relative_place = i + 1 - double_places_counter
-                else:
+                    prev_split_time = leg.relative_time
+        else:
+            # calculate places for specified leg
+            leader_time = self.person_splits[0].get_leg_time(index)
+
+            double_places_counter = 0
+            prev_split_time = leader_time
+            for i in range(len(self.person_splits)):
+                person = self.person_splits[i]
+                leg = person.get_leg_by_course_index(index)
+                if leg:
+                    if i != 0 and prev_split_time == leg.leg_time:
+                        double_places_counter += 1
+                    else:
+                        double_places_counter = 0
+
                     leg.leg_place = i + 1 - double_places_counter
                     leg.leader_time = leader_time
-                prev_split_time = leg.leg_time
+                    prev_split_time = leg.leg_time
 
     def set_leg_leader(self, index, person_split):
         self.leader[str(index)] = (person_split.person.name, person_split.get_leg_time(index))
