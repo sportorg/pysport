5caab83111efd0984cce73445900ace2bc1c0e82
Semyon Yakimov <sdyakimov@gmail.com>
2020-08-24 19:32:44 +0300

    Fix bug with file creating

--- sportorg/gui/main_window.py
+++ sportorg/gui/main_window.py
@@ -467,7 +467,7 @@ class MainWindow(QMainWindow):
             _('SportOrg file (*.json)'),
             time.strftime("%Y%m%d")
         )
-        if not file_name:
+        if file_name:
             try:
                 if update_data:
                     new_event([Race()])
