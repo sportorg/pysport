a315a8e209734bc7f0a0de0dd53694b5ebd802ba
sergeikobelev <lorez@mail.ru>
2020-10-31 18:33:55 +0500

    Print splits if no course found (any course, dsq) (#387)

--- sportorg/modules/printing/model.py
+++ sportorg/modules/printing/model.py
@@ -3,7 +3,7 @@ import platform
 from sportorg.common.template import get_text_from_file
 from sportorg.config import template_dir
 from sportorg.language import translate
-from sportorg.models.memory import Organization, race
+from sportorg.models.memory import Organization, race, Group, Course
 from sportorg.models.result.split_calculation import GroupSplits
 from sportorg.modules.configs.configs import Config
 from sportorg.modules.printing.printing import print_html
@@ -21,65 +21,77 @@ class NoPrinterSelectedException(Exception):
 def split_printout(result):
     person = result.person
 
-    if not person or not person.group:
-        raise NoResultToPrintException('No results to print')
-
     obj = race()
     course = obj.find_course(result)
+    group = None
+    organization = None
 
-    if person.group and course:
-        printer = Config().printer.get('split')
-        template_path = obj.get_setting(
-            'split_template', template_dir('split', '1_split_printout.html')
-        )
-
-        s = GroupSplits(obj, person.group).generate(True)
-        result.check_who_can_win()
-
-        if not str(template_path).endswith('.html') and platform.system() == 'Windows':
-            # Internal split printout, pure python. Works faster, than jinja2 template + pdf
-
-            size = 60  # base scale factor is 60, used win32con.MM_TWIPS MapMode (unit = 1/20 of dot, 72dpi)
-
-            array = str(template_path).split(translate('scale') + '=')
-            if len(array) > 1:
-                scale = array[1]
-                if scale.isdecimal():
-                    size = int(scale) * size // 100
-
-            pr = SportorgPrinter(
-                printer,
-                size,
-                int(obj.get_setting('print_margin_left', 5.0)),
-                int(obj.get_setting('print_margin_top', 5.0)),
-            )
-
-            pr.print_split(result)
-            pr.end_doc()
-
-            return
-
+    if person:
+        group = person.group
         organization = person.organization
-        if not organization:
-            organization = Organization()
 
-        template = get_text_from_file(
-            template_path,
-            race=obj.to_dict(),
-            person=person.to_dict(),
-            result=result.to_dict(),
-            group=person.group.to_dict(),
-            course=course.to_dict(),
-            organization=organization.to_dict(),
-            items=s.to_dict(),
-        )
-        if not printer:
-            raise NoPrinterSelectedException('No printer selected')
-        print_html(
+    if not group:
+        group = Group()
+
+    if not course:
+        course = Course()
+
+    s = GroupSplits(obj, group).generate(True)
+
+    if not organization:
+        organization = Organization()
+
+    if not person or not person.group or not course:
+        # print the list of controls without checking
+        pass
+
+    printer = Config().printer.get('split')
+    template_path = obj.get_setting(
+        'split_template', template_dir('split', '1_split_printout.html')
+    )
+
+    result.check_who_can_win()
+
+    if not str(template_path).endswith('.html') and platform.system() == 'Windows':
+        # Internal split printout, pure python. Works faster, than jinja2 template + pdf
+
+        size = 60  # base scale factor is 60, used win32con.MM_TWIPS MapMode (unit = 1/20 of dot, 72dpi)
+
+        array = str(template_path).split(translate('scale') + '=')
+        if len(array) > 1:
+            scale = array[1]
+            if scale.isdecimal():
+                size = int(scale) * size // 100
+
+        pr = SportorgPrinter(
             printer,
-            template,
-            obj.get_setting('print_margin_left', 5.0),
-            obj.get_setting('print_margin_top', 5.0),
-            obj.get_setting('print_margin_right', 5.0),
-            obj.get_setting('print_margin_bottom', 5.0),
+            size,
+            int(obj.get_setting('print_margin_left', 5.0)),
+            int(obj.get_setting('print_margin_top', 5.0)),
         )
+
+        pr.print_split(result)
+        pr.end_doc()
+
+        return
+
+    template = get_text_from_file(
+        template_path,
+        race=obj.to_dict(),
+        person=person.to_dict(),
+        result=result.to_dict(),
+        group=group.to_dict(),
+        course=course.to_dict(),
+        organization=organization.to_dict(),
+        items=s.to_dict(),
+    )
+    if not printer:
+        raise NoPrinterSelectedException('No printer selected')
+    print_html(
+        printer,
+        template,
+        obj.get_setting('print_margin_left', 5.0),
+        obj.get_setting('print_margin_top', 5.0),
+        obj.get_setting('print_margin_right', 5.0),
+        obj.get_setting('print_margin_bottom', 5.0),
+    )
