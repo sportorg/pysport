94d0b562743bebec4037351b5f3ec8b8be4b0d6b
Alexandr Karpov <thiefsy@gmail.com>
2023-01-07 15:52:44 +0700

    correct() and incorrect() templates

--- tests/test_result_checking.py
+++ tests/test_result_checking.py
@@ -1,43 +1,43 @@
 from typing import List
-from sportorg.models.result.result_checker import ResultChecker
 from sportorg.models.memory import ResultSportident, Person, Course, CourseControl, Split
 
 
-def test_basic_check():
-    result = ResultSportident()
-    result.person = Person()
-    assert ResultChecker.checking(result)
-
-
-def make_course(course) -> Course:
+def make_course(course: List[int]) -> Course:
     course_object = Course()
-    course_object.controls = [course_control(code) for code in course]
+    course_object.controls = [make_course_control(code) for code in course]
     return course_object
 
-def course_control(code: int) -> CourseControl:
-    control_object = CourseControl()
-    control_object.update_data({'code': code, 'length': 0})
-    return control_object
+def make_course_control(code: int) -> CourseControl:
+    control = CourseControl()
+    control.update_data({'code': code, 'length': 0})
+    return control
 
-def make_result(split) -> ResultSportident:
+def make_result(splits: List[int]) -> ResultSportident:
     result = ResultSportident()
     result.person = Person()
-    result.splits = [split_control(code) for code in split]
+    result.splits = [make_split_control(code) for code in splits]
     return result
 
-def split_control(code: int) -> Split:
-    split_object = Split()
-    split_object.update_data({'code': code, 'time': 0})
-    return split_object
+def make_split_control(code: int) -> Split:
+    split = Split()
+    split.update_data({'code': code, 'time': 0})
+    return split
 
-def check(course: List, split: List) -> bool:
-    result = make_result(split)
+def check(course: List[int], splits: List[int]) -> bool:
+    result = make_result(splits)
     course_object = make_course(course)
-
     return result.check(course_object)
+
+def correct(course: List[int], splits: List[int]) -> bool:
+    return check(course, splits)
+
+def incorrect(course: List[int], splits: List[int]) -> bool:
+    return not check(course, splits)
 
 
 def test_advanced_check():
-    course = [31, 32, 33]
-    split = [31, 32, 33]
-    assert check(course, split)
+    assert correct(course=[31, 32, 33],
+                   splits=[31, 32, 33])
+
+    assert incorrect(course=[31, 32, 33],
+                     splits=[31, 32])
