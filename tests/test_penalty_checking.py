ab87d6c7f75db06c5dddd58f58c17ef12e22f582
Aleksandr Karpov <alex.nsk.karpov@gmail.com>
2023-01-11 20:07:20 +0700

    Improve helper functions for test_penalty_checking

--- tests/test_penalty_checking.py
+++ tests/test_penalty_checking.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Tuple, Union
 
 from sportorg.models.memory import (
     Course,
@@ -16,29 +16,17 @@ from sportorg.models.result.result_checker import ResultChecker
 
 def test_playground():
     create_race()
-    assert ok(course=[31, 32, '33'], splits=[31, 32, 33], penalty=0)
-    assert dsq(course=[31, 32, '33'], splits=[31, 32, 70], penalty=0)
+    assert ok(course=[31, 32, "33"], splits=[31, 32, 33], penalty=0)
+    assert dsq(course=[31, 32, "33"], splits=[31, 32, 70], penalty=0)
 
 
-def ok(course: List[Union[int, str]], splits: List[int], penalty: int) -> bool:
-    '''Проверка отметки: результат засчитан
-
-    Parameters
-    ----------
-    course : List[Union[int, str]]
-        Порядок прохождения дистанции
-    splits : List[int]
-        Отметки спортсмена на дистанции
-
-    Returns
-    -------
-    bool
-        Возвращает True если отметка признана правильной
-    '''
-    return check(course, splits, penalty)
-
-def dsq(course: List[Union[int, str]], splits: List[int], penalty: int) -> bool:
-    '''Проверка отметки: результат не засчитан, нарушение порядка прохождения дистанции
+def ok(
+    course: List[Union[int, str]],
+    splits: List[int],
+    result_status: ResultStatus = ResultStatus.OK,
+    penalty: int = 0,
+) -> bool:
+    """Проверка отметки: результат засчитан, начислено заданное количество штрафа
 
     Parameters
     ----------
@@ -46,20 +34,82 @@ def dsq(course: List[Union[int, str]], splits: List[int], penalty: int) -> bool:
         Порядок прохождения дистанции
     splits : List[int]
         Отметки спортсмена на дистанции
+    result_status: ResultStatus,
+        Ожидаемый результат проверки. По умолчанию result_status=ResultStatus.OK
+    penalty : int
+        Ожидаемое количество штрафа. Минуты или штрафные круги — зависит от
+        заранее заданных настроек.
 
     Returns
     -------
     bool
-        Возвращает True если отметка признана неправильной
-    '''
-    return not check(course, splits, penalty)
+        Возвращает True если отметка признана правильной и штраф совпадает
+    """
+    return all(check(course, splits, result_status=result_status, penalty=penalty))
 
-def check(course: List[Union[int, str]], splits: List[int], penalty: int) -> bool:
+
+def dsq(
+    course: List[Union[int, str]],
+    splits: List[int],
+    result_status: ResultStatus = ResultStatus.MISSING_PUNCH,
+    penalty: int = 0,
+) -> bool:
+    """Проверка отметки: результат не засчитан, нарушение порядка прохождения
+    дистанции, но штраф посчитан верно.
+
+    Parameters
+    ----------
+    course : List[Union[int, str]]
+        Порядок прохождения дистанции
+    splits : List[int]
+        Отметки спортсмена на дистанции
+    result_status: ResultStatus,
+        Ожидаемый результат проверки. По умолчанию result_status=ResultStatus.MISSING_PUNCH
+    penalty : int
+        Ожидаемое количество штрафа. Минуты или штрафные круги — зависит от
+        заранее заданных настроек.
+
+    Returns
+    -------
+    bool
+        Возвращает True если отметка признана неправильной, но штраф посчитан верно
+    """
+    return all(check(course, splits, result_status=result_status, penalty=penalty))
+
+
+def check(
+    course: List[Union[int, str]],
+    splits: List[int],
+    result_status: ResultStatus = ResultStatus.OK,
+    penalty: int = 0,
+) -> Tuple[bool, bool]:
+    """Проверка отметки с начислением штрафа. Параметры проверки задаются
+    заранее и хранятся в объекте race().get_setings().
+
+    Parameters
+    ----------
+    course : List[Union[int, str]]
+        Порядок прохождения дистанции
+    splits : List[int]
+        Отметки спортсмена на дистанции
+    result_status: ResultStatus,
+        Ожидаемый результат проверки. По умолчанию result_status=ResultStatus.OK
+    penalty : int
+        Ожидаемое количество штрафа. Минуты или штрафные круги — зависит от
+        заранее заданных настроек. По умолчанию penalty=0
+
+    Returns
+    -------
+    Tuple[bool, bool]
+        Возвращает кортеж: результат проверки правильности прохождения и
+        результат сравнения начисленного штрафа с ожидаемым.
+    """
     race().courses[0].controls = make_course_controls(course)
     result = race().results[0]
     result.splits = make_splits(splits)
     ResultChecker.checking(result)
-    return result.status == ResultStatus.OK and result.penalty_laps == penalty
+    return result.status == result_status, result.penalty_laps == penalty
+
 
 def create_race():
     course = create(Course)
@@ -72,18 +122,22 @@ def create_race():
     race().persons.append(person)
     race().results.append(result)
 
+
 def make_course(course: List[Union[int, str]]) -> Course:
     course_object = Course()
     course_object.controls = make_course_controls(course)
     return course_object
 
+
 def make_course_controls(course: List[Union[int, str]]) -> List[CourseControl]:
     return [make_course_control(code) for code in course]
 
+
 def make_course_control(code: Union[int, str]) -> CourseControl:
     control = CourseControl()
-    control.update_data({'code': code, 'length': 0})
+    control.update_data({"code": code, "length": 0})
     return control
+
 
 def make_splits(splits: List[int]) -> List[Split]:
     return [create(Split, code=str(code)) for code in splits]
