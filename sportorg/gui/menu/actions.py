84460378c16e342b65a2e95bd94a13c647c60c95
Aleksandr Karpov <alex.nsk.karpov@gmail.com>
2023-11-28 16:36:11 +0700

    fix: add missing checking()

--- sportorg/gui/menu/actions.py
+++ sportorg/gui/menu/actions.py
@@ -593,6 +593,7 @@ class PenaltyCalculationAction(Action, metaclass=ActionFactory):
         logging.debug('Penalty calculation start')
         for result in race().results:
             if result.person:
+                ResultChecker.checking(result)
                 ResultChecker.calculate_penalty(result)
                 ResultChecker.checking(result)
         logging.debug('Penalty calculation finish')
