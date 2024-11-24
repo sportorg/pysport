082581e905a86f4ae79d281c73c0e7e9b1a98963
Danil Akhtarov <daxartio@gmail.com>
2024-11-24 17:19:38 +0400

    feat: add compressing and decompressing json files

--- sportorg/modules/backup/file.py
+++ sportorg/modules/backup/file.py
@@ -1,3 +1,4 @@
+import gzip
 import logging
 from typing import Optional
 
@@ -13,33 +14,42 @@ logger = logging.getLogger(__name__)
 class File:
     def __init__(self, file_name: str):
         self._file_name = file_name
+        self.use_utf8 = Config().configuration.get("save_in_utf8", False)
+        self.use_gzip = Config().configuration.get("save_in_gzip", False)
 
-    @staticmethod
-    def backup(file_name: str, func, mode: str = "r") -> None:
-        use_utf8 = Config().configuration.get("save_in_utf8", False)
+    def _backup(self, file_name: str, func, mode: str = "r") -> None:
+        use_utf8 = self.use_utf8
+        use_gzip = self.use_gzip
         # if user set UTF-8 usage, first try to open file in UTF-8,
         # then in system locale (1251 for RU Windows)
-        try:
-            def_encoding = None
-            if use_utf8:
-                def_encoding = "utf-8"
 
+        if mode == "r":
+            try:
+                with gzip.open(file_name) as f:
+                    f.read(1)
+                use_gzip = True
+            except gzip.BadGzipFile:
+                use_gzip = False
+
+        def_encoding = "utf-8" if use_utf8 else None
+        if use_gzip:
+            mode = f"{mode}+b"
+
+        try:
             with open(file_name, mode, encoding=def_encoding) as f:
-                func(f)
+                func(f, compress=use_gzip)
 
         except UnicodeDecodeError:
             f.close()
 
-            alt_encoding: Optional[str] = "utf-8"
-            if use_utf8:
-                alt_encoding = None
+            alt_encoding: Optional[str] = "utf-8" if use_utf8 else None
 
             with open(file_name, mode, encoding=alt_encoding) as f:
-                func(f)
+                func(f, compress=use_gzip)
 
     def create(self) -> None:
         logger.info("Create " + self._file_name)
-        self.backup(
+        self._backup(
             self._file_name + ".tmp",
             json.dump,
             "w",
@@ -48,7 +58,7 @@ class File:
 
     def save(self) -> None:
         logger.info("Save " + self._file_name)
-        self.backup(
+        self._backup(
             self._file_name + ".tmp",
             json.dump,
             "w",
@@ -57,7 +67,7 @@ class File:
 
     def open(self) -> None:
         logger.info("Open " + self._file_name)
-        self.backup(
+        self._backup(
             self._file_name,
             json.load,
             "r",
--- tests/test_gzip.py
+++ tests/test_gzip.py
@@ -1,9 +1,23 @@
+import pytest
+
 from sportorg.models.memory import Group, Organization, Person, race
 from sportorg.modules.backup.file import File
+from sportorg.modules.configs.configs import Config
 
 
-def test_open_json_file():
+@pytest.fixture()
+def use_gzip():
+    old_value = Config().configuration.get("save_in_gzip", False)
+    Config().configuration.set("save_in_gzip", True)
+    yield
+    Config().configuration.set("save_in_gzip", old_value)
+
+
+@pytest.mark.usefixtures('use_gzip')
+def test_save_and_open_gzip():
     File("tests/data/test.json").open()
+    File("tests/data/test.json.tmp").save()
+    File("tests/data/test.json.tmp").open()
     r = race()
     person = None
     for p in r.persons:
