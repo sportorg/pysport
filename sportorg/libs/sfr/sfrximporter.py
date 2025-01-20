d17df20b166c7ef30999ce17d842b1174ece6ac3
v.filatov <v.filatov@agisoft.com>
2025-01-20 09:37:22 +0300

    Import sfrx file

---
+++ sportorg/libs/sfr/sfrximporter.py
@@ -0,0 +1,233 @@
+import logging
+
+from sportorg.gui.dialogs.text_io import set_property
+from sportorg.language import translate
+from sportorg.libs.sfr import sfrxparser
+from sportorg.models import memory
+from sportorg.models.memory import Qualification, race, ResultManual, ResultStatus, Split, SystemType
+from sportorg.utils.time import hhmmss_to_time
+
+
+def import_sfrx(source):
+    sfr_csv = sfrxparser.parse(source)
+    obj = memory.race()
+
+    old_lengths = obj.get_lengths()
+
+    settings = sfr_csv.settings
+    race_type = settings["race_type"]
+
+    obj.data.race_type = race_type
+    obj.data.title = settings["title"]
+    obj.data.location = settings["location"]
+
+    for course in iter(sfr_csv.dists.values()):
+        name = course["name"]
+        bib = convert_bib(course["bib"])
+        if bib != 0:
+            name = str(bib)
+        if memory.find(memory.race().courses, name=name) is None:
+            c = memory.create(
+                memory.Course,
+                bib=bib,
+                name=name,
+                length=int(course["length"]),
+                climb=int(course["climb"]),
+            )
+            controls = []
+            for order, control in enumerate(course["controls"]):
+                if str(control["code"]).isdecimal():  # don't use start and finish
+                    controls.append(
+                        memory.create(
+                            memory.CourseControl,
+                            code=control["code"],
+                            order=order,
+                            length=control["length"]
+                        )
+                    )
+            c.controls = controls
+            memory.race().courses.append(c)
+
+    for group_val in iter(sfr_csv.groups.values()):
+        group_name = group_val["name"]
+        group = memory.find(obj.groups, name=group_name)
+        if group is None:
+            group = memory.Group()
+            group.name = group_name
+            group.long_name = group_name
+            obj.groups.append(group)
+        if group_val["course"] != -1:
+            course = sfr_csv.dists[str(group_val["course"])]
+            course = memory.find(memory.race().courses, name=course["name"])
+            group.course = course
+
+        group.race_type = race_type
+
+    for team_name in iter(sfr_csv.teams.values()):
+        org = memory.find(obj.organizations, name=team_name)
+        if org is None:
+            org = memory.Organization()
+            org.name = team_name
+            obj.organizations.append(org)
+
+    for person_dict in sfr_csv.data:
+
+        person = memory.Person()
+        person.name = person_dict["name"]
+        person.surname = person_dict["surname"]
+
+        bib = convert_bib(str(person_dict["bib"]))
+
+        person.set_bib(bib)
+        person.set_year(person_dict["year"])
+        group = person_dict["group_id"]
+
+        if int(group) >= 0:
+            person.group = memory.find(obj.groups, name=sfr_csv.groups[group]["name"])
+
+        team = person_dict["team_id"]
+        if int(team) >= 0:
+            person.organization = memory.find(obj.organizations, name=sfr_csv.teams[team])
+
+        if person_dict["qual_id"] and person_dict["qual_id"].isdigit():
+            qual_id = sfr_qual_to_sportorg()[person_dict["qual_id"]]
+        else:
+            qual_id = 0
+
+        person.qual = Qualification(qual_id)
+        person.comment = person_dict["comment"]
+
+        finish = person_dict["finish"]
+        if finish != "":
+            set_property(
+                person,
+                translate("Finish"),
+                finish,
+                creating_new_result=True)
+        credit = person_dict["credit"]
+        if credit != "":
+            set_property(
+                person,
+                translate("Credit"),
+                credit,
+                creating_new_result=True)
+
+        start = person_dict["start"]
+        if start != "":
+            set_property(
+                person,
+                translate("Start"),
+                start
+            )
+        result = person_dict["result"].strip()
+
+        if result != "" and len(result.split(":")) != 3:
+            result_person = race().find_person_result(person)
+            if result is None:
+                result_person = race().new_result(ResultManual)
+                result_person.person = person
+                result_person.bib = person.bib
+                race().add_new_result(result_person)
+
+            if result == "cнят":
+                result_person.status = ResultStatus.DISQUALIFIED
+            if result == "cнят (запр.)":
+                result_person.status = ResultStatus.DISQUALIFIED
+            if result == "cнят (к/в)":
+                result_person.status = ResultStatus.OVERTIME
+            if result == "н/c":
+                result_person.status = ResultStatus.DID_NOT_START
+
+        obj.persons.append(person)
+
+        result_person = race().find_person_result(person)
+        if result_person:
+            result_person.system_type = SystemType.SFR
+
+    for split_csv in sfr_csv.splits:
+        bib = convert_bib(split_csv["bib"])
+        person = race().find_person_by_bib(bib)
+        if person is None:
+            continue
+        result_person = race().find_person_result(person)
+        if result_person is None:
+            continue
+
+        splits = []
+        for split_csv_value in split_csv["split"]:
+            split = Split()
+
+            code = split_csv_value[0]
+            if code == "240" or code == "241":
+                continue
+
+            split.code = code
+
+            split.time = hhmmss_to_time(split_csv_value[1])
+            splits.append(split)
+        result_person.splits = splits
+
+    new_lengths = obj.get_lengths()
+
+    logging.info(translate("Import result"))
+    logging.info("{}: {}".format(translate("Persons"), new_lengths[0] - old_lengths[0]))
+    # logging.info('{}: {}'.format(translate('Race Results'), new_lengths[1]-old_lengths[1]))
+    logging.info("{}: {}".format(translate("Groups"), new_lengths[2] - old_lengths[2]))
+    # logging.info('{}: {}'.format(translate('Courses'), new_lengths[3]-old_lengths[3]))
+    logging.info("{}: {}".format(translate("Teams"), new_lengths[4] - old_lengths[4]))
+
+    persons_dupl_cards = obj.get_duplicate_card_numbers()
+    persons_dupl_names = obj.get_duplicate_names()
+
+    if len(persons_dupl_cards):
+        logging.info(
+            "{}".format(translate("Duplicate card numbers (card numbers are reset)"))
+        )
+    for person in sorted(persons_dupl_cards, key=lambda x: x.card_number):
+        logging.info(
+            "{} {} {} {}".format(
+                person.full_name,
+                person.group.name if person.group else "",
+                person.organization.name if person.organization else "",
+                person.card_number,
+            )
+        )
+        person.set_card_number(0)
+    if len(persons_dupl_names):
+        logging.info("{}".format(translate("Duplicate names")))
+    for person in sorted(persons_dupl_names, key=lambda x: x.full_name):
+        logging.info(
+            "{} {} {} {}".format(
+                person.full_name,
+                person.get_year(),
+                person.group.name if person.group else "",
+                person.organization.name if person.organization else "",
+            )
+        )
+
+
+def convert_bib(bib):
+    if not bib.isdigit():
+        return 0
+
+    if int(bib) < 10000:
+        return int(bib)
+    else:
+        return int(bib[0] + str(int(bib[1:])))
+
+
+def sfr_qual_to_sportorg():
+    return {
+        "": 0,
+        "0": 0,
+        "1": 3,
+        "2": 2,
+        "3": 1,
+        "4": 6,
+        "5": 5,
+        "6": 4,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 8
+    }
---
+++ tests/test_sfrx.py
@@ -0,0 +1,23 @@
+from sportorg.libs.sfr import sfrximporter
+from sportorg.models.memory import (
+    Group,
+    Organization,
+    Person,
+    Qualification,
+    find,
+    race,
+)
+from sportorg.modules.winorient import winorient
+
+
+def test_import_sfrx():
+    sfrximporter.import_sfrx("tests/data/sfrx_file.sfrx")
+    person = find(race().persons, name="СЕМЕН", surname="НИКИТИН")
+    assert isinstance(person, Person), "Not person"
+    assert isinstance(person.group, Group), "Not group"
+    assert person.group.name == "М14", "Group name error"
+    assert isinstance(person.organization, Organization), "Not organization"
+    assert person.organization.name == "СДЮСШ Балт. берег Peterhof", "Organization name error"
+    assert person.get_year() == 2009, "Year error"
+    assert person.card_number == 0, "Card number error"
+    assert person.qual == Qualification.I_Y, "Qualification error"
