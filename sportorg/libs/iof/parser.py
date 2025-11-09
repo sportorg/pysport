725277ddcdf12c6f68ba6bd11d24ea1f0feba0ec
Sergei <lorez@mail.ru>
2025-11-09 19:20:41 +0500

    feat: read comments and one-man-relay variation from IOF XML

--- sportorg/libs/iof/parser.py
+++ sportorg/libs/iof/parser.py
@@ -98,7 +98,7 @@ def variation_data(tree, ns):
     root = tree.getroot()
     if "CourseData" not in root.tag:
         return
-    teams = []
+    course_assignments = []
 
     version = "0"
     if "iofVersion" in root.attrib:
@@ -119,9 +119,17 @@ def variation_data(tree, ns):
                     "course_name": leg_el.find("iof:CourseName", ns).text
                 }
                 team["legs"].append(leg)
-            teams.append(team)
+            course_assignments.append(team)
 
-    return teams
+        for person_el in root.find("iof:RaceCourseData", ns).findall("iof:PersonCourseAssignment", ns):
+            course_assignment = {
+                "bib_number": int(person_el.find("iof:BibNumber", ns).text),
+                "course_name": person_el.find("iof:CourseName", ns).text,
+                "legs": []
+            }
+            course_assignments.append(course_assignment)
+
+    return course_assignments
 
 
 def entry_list(tree, ns):
@@ -161,6 +169,12 @@ def entry_list(tree, ns):
                 bib_el = extensions_el.find("orgeo:BibNumber", ns)
                 if bib_el is not None:
                     person["extensions"]["bib"] = bib_el.text
+
+            service_request_el = person_entry_el.find("iof:ServiceRequest", ns)
+            if service_request_el is not None:
+                comment_el = service_request_el.find("iof:Comment", ns)
+                if comment_el is not None:
+                    person["comment"] = comment_el.text
 
             org_el = person_entry_el.find("iof:Organisation", ns)
             organization = None
